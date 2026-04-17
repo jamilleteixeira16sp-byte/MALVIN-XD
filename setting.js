e2d2e109c9c83d4c7be314e6a2ba4021f7d55975
jamilleteixeira16sp-byte <jamilleteixeira16sp@gmail.com>
2026-04-17 12:59:00 -0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,10 +1,10 @@
 //=====🏅 ᴍᴀʟᴠɪɴ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
-  SESSION_ID: '', // add your session ID
+  BOT_NAME: 'Millasbot', // ur bot name
+  OWNER_NAME: 'milla', //your name
+  OWNER_NUMBER: '5511964472295', // add your phone number
+  SESSION_ID: 'king~IuASzBny1fpX', // add your session ID
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare' // ur timezone
 };
